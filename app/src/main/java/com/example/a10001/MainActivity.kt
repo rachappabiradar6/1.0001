645e4f263b5f177a8907676962f9690016551917
rachaappa biradar <rachappabiradar6@gmail.com>
2021-10-22 01:09:14 +0530

    logo to register page going activity

--- app/src/main/java/com/example/a10001/MainActivity.kt
+++ app/src/main/java/com/example/a10001/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.a10001
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import java.security.PrivateKey
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageview:ImageView= findViewById(R.id.imageView)
+        imageview.setOnClickListener{
+            nextpageactivity()
+        }
+    }
+
+    private fun nextpageactivity() {
+        val intent =Intent(this,Register::class.java)
+        startActivity(intent)
+
     }
 }
